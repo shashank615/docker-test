a617cddeadd685ab815b2198bf4a0f1b37652d15
nainish <pmo_dbalounge@shubham.co>
2025-05-22 15:07:29 +0530

    api developed

--- src/main/java/com/example/docker_test/Controller.java
+++ src/main/java/com/example/docker_test/Controller.java
@@ -1,2 +1,14 @@
-package com.example.docker_test;public class Controller {
+package com.example.docker_test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/index")
+    String getHomePage()
+    {
+        return "index";
+    }
+
+    //helloe
 }
